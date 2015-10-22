ed2818c7ed50bac7abaded75d934ab3b7a7adc2e
gardners <paul@servalproject.org>
2015-10-22 14:29:08 +1030

    give status file more appropriate name.

--- status_dump.c
+++ status_dump.c
@@ -43,7 +43,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "serial.h"
 #include "version.h"
 
-#define STATUS_FILE "/tmp/lbard.status"
+#define STATUS_FILE "/tmp/lbard_status.html"
 
 int status_dump()
 {
