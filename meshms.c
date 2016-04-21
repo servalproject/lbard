a2df9f54daa5b0c2be765500b39429e4312a757d
gardners <paul@servalproject.org>
2016-04-21 13:40:48 +0930

    work towards meshms command line options working.

--- meshms.c
+++ meshms.c
@@ -44,14 +44,48 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "sync.h"
 #include "lbard.h"
 
+/* Find http api username and password, and also http port for us to connect to.
+ */
+char server_and_port[1024];
+char auth_token[1024];
+
+int meshms_parse_serval_conf()
+{
+  char serval_conf_file[1024];
+  char auth_name[1024]="username";
+  char auth_key[1024]="password";
+  int port=4110;
+  
+  snprintf(serval_conf_file,1024,"%s/serval.conf",getenv("SERVALINSTANCE_PATH"));
+  FILE *f=fopen(serval_conf_file,"r");
+  char line[1024];
+  if (f) {
+    line[0]=0; fgets(line,1024,f);
+    while(line[0]) {
+      sscanf(line,"api.restful.users.%[^.].password=%s",
+	     auth_name,auth_key);
+      sscanf(line,"rhizome.http.port=%d",&port);
+      line[0]=0; fgets(line,1024,f);
+    }
+    fclose(f);
+  }
+
+  snprintf(server_and_port,1024,"127.0.0.1:%d",port);
+  snprintf(auth_token,1024,"%s:%s",auth_name,auth_key);
+  return 0;
+}
+
+
 int meshms_list_conversations(char *sid_hex)
 {
-  return 0;
+  return http_list_meshms_conversations(server_and_port,auth_token,
+					sid_hex,30000);
 }
 
 int meshms_list_messages(char *sender_sid_hex, char *recipient_sid_hex)
 {
-  return 0;
+  return http_list_meshms_messages(server_and_port,auth_token,
+				   sender_sid_hex,recipient_sid_hex,30000);
 }
 
 int meshms_send_message(char *sender_sid_hex, char *recipient_sid_hex,
@@ -72,6 +106,8 @@ int meshms_usage()
 
 int meshms_parse_command(int argc,char **argv)
 {
+  meshms_parse_serval_conf();
+  if (argc<3) exit(meshms_usage());
   if (argc<=7) {
     if (!strcasecmp(argv[2],"list")) {
       if (!strcasecmp(argv[3],"conversations")) {
