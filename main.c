0f7c85f0e9dea1b8bc16d901cd1647e58a125dc9
gardners <paul@servalproject.org>
2017-03-23 13:18:28 +1030

    bump version

--- main.c
+++ main.c
@@ -124,7 +124,7 @@ long long start_time=0;
 
 int main(int argc, char **argv)
 {
-  fprintf(stderr,"Version 20170323.1104.1\n");
+  fprintf(stderr,"Version 20170323.1319.1\n");
   
   start_time = gettime_ms();
   
