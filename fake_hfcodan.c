c3ee55808fa578c838bc4b2ac450b0b2a72a5f6a
gardners <paul@servalproject.org>
2017-05-05 05:16:44 +0930

    add control-U support and ignore 0x00 chars in command buffer

--- fake_hfcodan.c
+++ fake_hfcodan.c
@@ -3,7 +3,11 @@
 
 int hfcodan_read_byte(int i,unsigned char c)
 {
-  if (c!='\n'&&c!='\r') {
+  if (c==0x15) {
+    // Control-U -- clear input buffer
+    clients[i].buffer_count=0;
+  } else if (c!='\n'&&c!='\r'&&c) {
+    // fprintf(stderr,"Radio #%d received character 0x%02x\n",i,c);    
     if (clients[i].buffer_count<(CLIENT_BUFFER_SIZE-1))
       clients[i].buffer[clients[i].buffer_count++]=c;
   } else {
