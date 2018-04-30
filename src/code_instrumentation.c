4824aecacc76691dada297826248713971482c49
Kris Coppieters <kris@rorohiko.com>
2018-04-30 18:02:20 +1200

    Add instrumentation module

---
+++ src/code_instrumentation.c
@@ -0,0 +1,16 @@
+#include "code_instrumentation.h"
+
+void code_instrumentation_log(const char* fileName, int line, const char* functionName, int logLevel, const char *msg, ...)
+{
+
+}
+
+void code_instrumentation_entry(const char* functionName)
+{
+
+}
+
+void code_instrumentation_exit(const char* functionName)
+{
+
+}
