d996f3a0c0a6430c33c3538e27fff5d3aba45ff6
Maitre-Hiboux <maxime.prost39@gmail.com>
2018-09-17 18:26:33 +0930

    begin implementing fake rfd lora radio

--- src/drivers/fake_alora.c
+++ src/drivers/fake_alora.c
@@ -2,8 +2,45 @@
 
 #include "fakecsmaradio.h"
 
+int last_was_lf=0;
 int rfdlora_read_byte(int i,unsigned char c)
 {
+  if (c=='\n') last_was_lf=1; else last_was_lf=0;
+  if ((c=='\r')&&last_was_lf) {
+    // Have line of input 
+        if (clients[i].buffer_count) {
+      
+      clients[i].buffer[clients[i].buffer_count]=0;
+      fprintf(stderr,"Codan HF Radio #%d sent command '%s'\n",i,clients[i].buffer);
+
+      // Process the command here
+      if (!strcasecmp("mac pause",(char *)clients[i].buffer)) {
+        // Stop MAC from receiving (we will ignore any packets that come, until we resume it
+      } else if (!strcasecmp("radio rx 0",(char *)clients[i].buffer)) {
+        // Resume receiving packets
+      } else if (!strcasecmp("sys get pindig GPIO13",(char *)clients[i].buffer)) {
+        // 
+      } else if (!strcasecmp("sys set pindig GPIO13 1",(char *)clients[i].buffer)) {
+        // 
+      } else if (!strcasecmp("sys set pindig GPIO13 0",(char *)clients[i].buffer)) {
+        // 
+      } else if (!strcasecmp("sys reset",(char *)clients[i].buffer)) {
+        write(clients[i].socket,"RN2903 1.0.3 Aug  8 2017 15:11:09\r\n",strlen("RN2903 1.0.3 Aug  8 2017 15:11:09\r\n"));
+      } else if (!strcasecmp("",(char *)clients[i].buffer)) {
+        // 
+      } else {
+        write(clients[i].socket,"invalid param\r\n",strlen("invalid param\r\n"));
+      }
+      // Reset buffer ready for next command
+      clients[i].buffer_count=0;
+    }    
+
+  } else {
+    // fprintf(stderr,"Radio #%d received character 0x%02x\n",i,c);
+   
+    if (clients[i].buffer_count<(CLIENT_BUFFER_SIZE-1))
+      clients[i].buffer[clients[i].buffer_count++]=c;
+  }
   return 0;
 }
 
