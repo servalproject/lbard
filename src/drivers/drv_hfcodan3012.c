32347c33c8ab4a655e46855366b3ee1d5b1d749c
Paul Gardner-Stephen <paul@servalproject.org>
2019-11-29 15:30:25 +1030

    give Codan 3012 driver a unique short identifier

--- src/drivers/drv_hfcodan3012.c
+++ src/drivers/drv_hfcodan3012.c
@@ -4,7 +4,7 @@ The following specially formatted comments tell the LBARD build environment abou
 See radio_type for the meaning of each field.
 See radios.h target in Makefile to see how this comment is used to register support for the radio.
 
-RADIO TYPE: HFCODAN,"hfcodan3012","Codan HF with 3012 Data Modem",hfcodan3012_radio_detect,hfcodan3012_serviceloop,hfcodan3012_receive_bytes,hfcodan3012_send_packet,hfcodan32_radio_check_if_ready,10
+RADIO TYPE: HFCODAN3012,"hfcodan3012","Codan HF with 3012 Data Modem",hfcodan3012_radio_detect,hfcodan3012_serviceloop,hfcodan3012_receive_bytes,hfcodan3012_send_packet,hfcodan32_radio_check_if_ready,10
 
 */
 
